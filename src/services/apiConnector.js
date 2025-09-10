a0e8b5f5b66f83b341ad261774b16789454749b8
kartiik22 <kartikspics@gmail.com>
2025-09-11 05:26:53 +0530

    cors

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -3,8 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
     withCredentials: true,
     headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        'Content-Type': 'application/json'
     },
     validateStatus: function (status) {
         return status >= 200 && status < 500; // Handle all statuses except server errors
@@ -12,16 +11,11 @@ export const axiosInstance = axios.create({
 });
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
-  const defaultHeaders = {
-    'Access-Control-Allow-Origin': 'https://smashicse-edtech-platform-1.onrender.com',
-    'Access-Control-Allow-Credentials': 'true'
-  };
-
   return axiosInstance({
     method: `${method}`,
     url: `${url}`,
     data: bodyData ? bodyData : null,
-    headers: { ...defaultHeaders, ...(headers || {}) },
+    headers: headers || {},
     params: params ? params : null,
   });
 };
